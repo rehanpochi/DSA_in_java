bb220c447bd981ca680ff998750e5f77ae359825
rehanpochi <rehanpochi1@gmail.com>
2022-01-05 14:43:19 +0530

    Selection Sort program.
    Signed-off-by: rehanpochi <rehanpochi1@gmail.com>

---
+++ src/com/java_programs/SelectionSort.java
@@ -0,0 +1,34 @@
+package com.java_programs;
+import java.util.Arrays;
+
+public class SelectionSort {
+    public static void main(String[] args) {
+        int[] arr = {5, 43, 3, 22, 1111};
+        selection(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void selection(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            int last=arr.length-i-1;
+            int maxIndex=findMaxIndex(arr,0,last);
+            swap(arr,maxIndex,last);
+        }
+    }
+
+    static void swap(int[] arr, int first, int second){
+        int temp=arr[first];
+        arr[first]=arr[second];
+        arr[second]=temp;
+    }
+
+    static int findMaxIndex(int[] arr,int start, int end){
+        int max=start;
+        for(int i=start;i<= end;i++){
+            if (arr[max]<arr[i]){
+                max=i;
+            }
+        }
+        return max;
+    }
+}
